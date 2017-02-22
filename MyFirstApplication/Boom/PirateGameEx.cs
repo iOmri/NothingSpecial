17c66d3e4186734007bfed3ddd8bb74e93ef4df6
iYehuda <yehudaac1@gmail.com>
2017-02-22 19:09:39 +0200

    Working on lists initialization

--- MyFirstApplication/Boom/PirateGameEx.cs
+++ MyFirstApplication/Boom/PirateGameEx.cs
@@ -1,5 +1,6 @@
 ﻿using Pirates;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Boom
 {
@@ -8,10 +9,13 @@ namespace Boom
         public PirateGame Game { get; set; }
 
 
-        public List<AircraftEx> MyAircrafts;
-        public List<AircraftEx> MyLivingAircrafts;
-        public List<AircraftEx> EnemyAircrafts;
-        public List<AircraftEx> EnemyLivingAircrafts;
+        public List<AircraftEx> MyAircrafts { get; set; }
+        public List<AircraftEx> MyLivingAircrafts { get; set; }
+        public List<AircraftEx> EnemyAircrafts { get; set; }
+        public List<AircraftEx> EnemyLivingAircrafts { get; set; }
+
+        public List<DroneEx> MyLivingDrones { get; set; }
+        public List<DroneEx> EnemyLivingDrones { get; set; }
 
         public List<PirateEx> MyPirates { get; set; }
         public List<PirateEx> MyLivingPirates { get; set; }
@@ -21,6 +25,52 @@ namespace Boom
         public PirateGameEx(PirateGame pirateGame)
         {
             Game = pirateGame;
+
+            MyPirates = CreateExList(Game.GetAllMyPirates());
+            MyLivingPirates = CreateExList(Game.GetMyLivingPirates());
+            EnemyPirates = CreateExList(Game.GetAllEnemyPirates());
+            EnemyLivingPirates = CreateExList(Game.GetEnemyLivingPirates());
+
+            MyLivingDrones = CreateExList(Game.GetMyLivingDrones());
+            EnemyLivingDrones = CreateExList(Game.GetEnemyLivingDrones());
+
+            MyLivingAircrafts = CreateExList(Game.GetMyLivingAircrafts());
+            MyAircrafts = ConvertList<PirateEx, AircraftEx>(MyPirates) +
+                ConvertList<DroneEx, AircraftEx>(MyLivingDrones);
+            EnemyLivingAircrafts = CreateExList(Game.GetEnemyLivingAircrafts());
+
+            
+        }
+
+        private List<AircraftEx> CreateExList(List<Aircraft> list)
+        {
+            return (from aircraft in list
+                    select new AircraftEx(aircraft)).ToList();
+        }
+
+        private List<DroneEx> CreateExList(List<Drone> list)
+        {
+            return (from drone in list
+                    select new DroneEx(drone)).ToList();
+        }
+
+        private List<PirateEx> CreateExList(List<Pirate> list)
+        {
+            return (from pirate in list
+                    select new PirateEx(pirate)).ToList();
+        }
+
+        private List<TDest> ConvertList<TSource, TDest>(List<TSource> list)
+            where TSource : TDest
+        {
+            List<TDest> result = new List<TDest>();
+            if (!typeof(TDest).IsSubclassOf(typeof(TSource)))
+                return result;
+
+            foreach (TSource src in list)
+                result.Add(src);
+
+            return result;
         }
     }
 }
