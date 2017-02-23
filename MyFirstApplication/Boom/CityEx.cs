a1435f2c9849f1e7cbc3377831e745eaf7b45b22
iYehuda <yehudaac1@gmail.com>
2017-02-23 02:13:55 +0200

    Added CityEx class Currently working on IsCamped method Prerequisites: Make PirateGameEx::ConvertList function static and public

---
+++ MyFirstApplication/Boom/CityEx.cs
@@ -0,0 +1,23 @@
+﻿using Pirates;
+
+namespace Boom
+{
+    public class CityEx : MapObjectEx
+    {
+        public City City { get; set; }
+
+        public CityEx(City city)
+            :base(city)
+        {
+            City = city;
+        }
+
+        public bool IsCamped(int range = -1)
+        {
+            if (range == -1)
+                range = City.UnloadRange;
+
+            // return CountNearbyObjects(GameManager.CurrentTurn.EnemyLivingPirates)
+        }
+    }
+}
