978537959a3691e7fe732ced0898019d0c6407b4
iYehuda <yehudaac1@gmail.com>
2017-02-23 02:39:34 +0200

    Working on IslandEx::GetClosestCity TODO: Implement this function Prerequisites: PirateGameEx must contain cities list

--- MyFirstApplication/Boom/IslandEx.cs
+++ MyFirstApplication/Boom/IslandEx.cs
@@ -1,6 +1,32 @@
-﻿namespace Boom
+﻿using Pirates;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Boom
 {
-    class IslandEx
+    public class IslandEx : MapObjectEx
     {
+        public Island Island { get; set; }
+        public List<PirateEx> SurroundingEnemies { get; set; }
+
+        /// <summary>
+        /// Creates a new IslandEx instance, initialized with a wrapped Island instance
+        /// </summary>
+        /// <param name="island">The wrapped Island instance</param>
+        public IslandEx(Island island)
+            : base(island)
+        {
+            Island = island;
+
+            // Filtered enemy living pirates list by their distance to the island
+            SurroundingEnemies = (from enemy in GameManager.CurrentTurn.EnemyLivingPirates
+                                  where Island.InControlRange(enemy.Pirate)
+                                  select enemy).ToList();
+        }
+
+        public CityEx GetClosestCity()
+        {
+
+        }
     }
 }
